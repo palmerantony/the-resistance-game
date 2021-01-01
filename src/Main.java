3dba42b0c6fd3bb00e9bbbfb81a6a13de1b14804
tonyp <palmerantony@gmail.com>
2021-01-01 09:07:22 +0000

    Add main method with comments outlining skeleton of how to play.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Accept player names
+
+        // Randomly select players to be spies, when commanded
+
+        // Inform spies who the other spies are
+
+        // Permit voting for Round 1
+
+        // Announce votes for Round 1
+
+        // Repeat for N rounds
+
+        System.out.println("Hello World!");
+    }
+}
